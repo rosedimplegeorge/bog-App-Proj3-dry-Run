63b2a0229ec91eaf46875f1078d0c029e80c61e5
rosedimple <rosedimplegeorge@gmail.com>
2018-04-26 23:09:57 -0400

    db Set up

---
+++ db/seeds.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const { Creature } = require('./schema')
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+// using Promises
+Creature.remove().then(() => {
+  const luke = new Creature({name: 'Luke', description: 'Jedi'})
+  return luke.save()
+}).then(() => {
+  const darth = new Creature({name: 'Darth Vader', description: 'Father of Luke'})
+  return darth.save()
+}).then(() => {
+  db.close()
+})
